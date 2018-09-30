10b1f7a91d88b5744140e317b5f69348a9c91136
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-30 15:07:34 -0700

    adding logic for 8/12 images

--- src/components/Game.js
+++ src/components/Game.js
@@ -10,7 +10,8 @@ import "./style.css";
 
 class Game extends React.Component {
     state = {
-        pictures:friends,
+        allPictures:friends,
+        pictures:friends.slice(0,8),
         score:0,
         highscore:0,
         message:"",
@@ -18,26 +19,55 @@ class Game extends React.Component {
     }
 
     componentDidMount() {
-        const newPictures = this.shuffle(this.state.pictures);
+        const newPictures = this.shuffle(this.state.allPictures);
         this.setState({pictures:newPictures});
     }
 
     handleClick = id => {
         const len= this.state.alreadyPicked.filter(idOfImage => idOfImage === id).length;
-        const newPictures = this.shuffle(this.state.pictures);
-        const newMessage= (len===0)?"Guessed Correctly":"Guessed Incorrectly";
-        const that = this;
-        (function() {
-            if(newMessage === "Guessed Incorrectly"){
-                setTimeout(()=> {
-                    that.setState({message:"Click to start again!"});
-                }, 1000);
-            }
-        }());
-        const newScore = (len===0)?this.state.score+1:0;
         const newAlreadyPicked = (len===0)?this.state.alreadyPicked.concat(id):[];
-        const newHighScore = (len===0)?(this.state.highscore<newScore)?newScore:this.state.highscore:this.state.highscore;
-        this.setState({message:newMessage, score:newScore, alreadyPicked:newAlreadyPicked, highscore:newHighScore, pictures:newPictures});
+        const newScore = (len===0)?this.state.score+1:0;
+        if(newScore<12){
+            this.setState({alreadyPicked:newAlreadyPicked},
+                () => 
+                { 
+                    let that = this;
+                    (function loop(){
+                        let newPictures = that.shuffle(that.state.allPictures); 
+                        if(newPictures.length===0)
+                            loop();
+                        else
+                            that.setState({pictures:newPictures})
+                    }());   
+                }
+            );
+            
+            const newMessage= (len===0)?"Guessed Correctly":"Guessed Incorrectly";
+            const that = this;
+            (function() {
+                if(newMessage === "Guessed Incorrectly"){
+                    setTimeout(()=> {
+                        that.setState({message:"Click to start again!"});
+                    }, 1000);
+                }
+            }());
+            
+            const newHighScore = (len===0)?(this.state.highscore<newScore)?newScore:this.state.highscore:this.state.highscore;
+            this.setState({message:newMessage, score:newScore, highscore:newHighScore});
+        }
+        else {
+            const that = this;
+            that.setState({message:"You won!Game resetting..",score:12, highscore:12 });
+            (function() {
+                setTimeout(()=> {
+                    that.setState({message:"",score:0,allPictures:friends,alreadyPicked :[]},
+                    ()=>{
+                        const newPictures = that.shuffle(that.state.allPictures);
+                        that.setState({pictures:newPictures});
+                    });
+                }, 1500);   
+            }());
+        }
     }
 
     shuffle = arr =>{
@@ -47,12 +77,18 @@ class Game extends React.Component {
             arr[i] = arr[j];
             arr[j] = x;
         }
-        return arr;
+        for(let j=0;j<8;j++){
+            const len = this.state.alreadyPicked.filter(id=> id===arr[j].id).length;
+            if(len===0)
+                return arr.slice(0,8);
+        }
+        return [];
+        
     }
 
     render(){
         return (
-                <Container cls={`${this.state.message === "Guessed Incorrectly" ? "animated shake" : this.state.message === "" ?"animated fadeInUp":""}`}>
+                <Container cls={`${this.state.message === "Guessed Incorrectly" ? "animated shake" : this.state.message === "" ?"animated fadeInUp": this.state.message ==="You won!Game resetting.."?"animated flash":""}`}>
                     <Row cls="game-heading">
                         <Col size="md-12">
                             <Span cls="game-name">CLICKY GAME</Span>
